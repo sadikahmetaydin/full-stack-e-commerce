4343a3bb7a0d277939a93346ed7d51d6e4a559e4
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-04-28 20:31:09 +0300

    create footer and policy component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Policy from "./components/Policy/Policy";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Policy />
+      <Footer />
     </>
   );
 };
