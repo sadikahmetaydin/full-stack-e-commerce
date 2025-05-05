5bac4accedb30279678e253ce0cbecfbc6f5b61a
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-05 21:15:05 +0300

    product detail page image slider functionality

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
 import productsData from "../../../data.json";
+import Slider from "react-slick";
 import "./Gallery.css";
+
+function NextBtn({ onClick }) {
+  return (
+    <button className="glide__arrow glide__arrow--right" data-glide-dir=">" onClick={onClick} style={{zIndex: "2"}}>
+        <i className="bi bi-chevron-right"></i>
+    </button>
+  )
+}
+
+function PrevBtn({ onClick }) {
+  return (
+    <button className="glide__arrow glide__arrow--left" data-glide-dir="<" onClick={onClick} style={{zIndex: "2"}}>
+        <i className="bi bi-chevron-left"></i>
+    </button>
+  )
+}
 
 const Gallery = () => {
 
   const [activeImg, setActiveImg] = useState(productsData[0].img.singleImage);
 
-
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+  }
 
   return (
     <div className="product-gallery">
@@ -16,24 +40,16 @@ const Gallery = () => {
         <div className="product-thumb">
             <div className="glide__track" data-glide-el="track">
               <ol className="gallery-thumbs glide__slides">
-
-                {
-                  productsData[0].img.thumbs.map((itemImg, index) => (
-                    <li className="glide__slide glide__slide--active" key={index} onClick={() => setActiveImg(itemImg)}>
-                      <img src={itemImg} alt="" className={`img-fluid ${itemImg === activeImg ? "active" : ""}`}/>
-                    </li>
-                  ))
-                }
-
+                <Slider {...sliderSettings}>
+                    {
+                      productsData[0].img.thumbs.map((itemImg, index) => (
+                        <li className="glide__slide glide__slide--active" key={index} onClick={() => setActiveImg(itemImg)}>
+                          <img src={itemImg} alt="" className={`img-fluid ${itemImg === activeImg ? "active" : ""}`}/>
+                        </li>
+                      ))
+                    }
+                </Slider>
               </ol>
-            </div>
-            <div className="glide__arrows" data-glide-el="controls">
-                <button className="glide__arrow glide__arrow--left" data-glide-dir="<">
-                    <i className="bi bi-chevron-left"></i>
-                </button>
-                <button className="glide__arrow glide__arrow--right" data-glide-dir=">">
-                    <i className="bi bi-chevron-right"></i>
-                </button>
             </div>
         </div>
     </div>
