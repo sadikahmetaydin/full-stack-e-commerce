b7023a7dcff77115faefa95e6182d73ee3da28a6
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-05 20:57:18 +0300

    productItem gallery active image functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <MainLayout>
       <App />
     </MainLayout>
-  </StrictMode>,
 )
