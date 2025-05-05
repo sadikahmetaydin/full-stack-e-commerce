a30bf7bcd43bcb91687a1cc56ffa9c376a9e769f
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-05 20:01:32 +0300

    create productItem slider and import json data of products

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import MainLayout from './layouts/MainLayout.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
