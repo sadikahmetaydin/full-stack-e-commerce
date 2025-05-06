8459c1005f7953992ca2ea3f87af2843020c5040
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-06 21:57:44 +0300

    cart localStorage process

--- frontend/src/context/CartProvider.jsx
+++ frontend/src/context/CartProvider.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = (cartItem) => {
     setCartItems((prevCart) => [...prevCart, cartItem]);
