50ab9270a305f28bfd467e1c011d93f827621243
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-12 22:35:38 +0300

    create new category functionality process

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category.js");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   res.send("Get all categories");
 });
+
+router.post("/", async (req, res) => {
+  try {
+    
+    const { name, img } = req.body;
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+    res.status(201).json(newCategory);
+
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
