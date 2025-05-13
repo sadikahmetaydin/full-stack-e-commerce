4bf8b0a0ec2307d8808a91dfe2c2c6b426c3e37d
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-13 19:08:04 +0300

    update category funtionality

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -47,4 +47,20 @@ router.get("/:categoryId", async (req, res) => {
  }
 });
 
+// Update category
+router.put("/:categoryId", async (req, res) => {
+  try {
+
+    const categoryId = req.params.categoryId;
+    const updates = req.body;
+    const existingCategory = await Category.findById(categoryId);
+    if(!existingCategory) return res.status(404).json({ error: "Category not found." });
+    const updatedCategory = await Category.findByIdAndUpdate(categoryId, updates, { new: true });
+    res.status(200).json(updatedCategory);
+    
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
