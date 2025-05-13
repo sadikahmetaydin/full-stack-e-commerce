82c169b2a1e1dba7b9397b431a1e4f886a4516e5
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-13 21:26:11 +0300

    create new user functionality

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const categoryRoute = require("./categories.js");
+const authRoute = require("./auth.js");
 const productRoute = require("./products.js");
 
 router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
 router.use("/products", productRoute);
 
 module.exports = router;
