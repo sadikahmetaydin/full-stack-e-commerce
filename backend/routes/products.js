939f8e9a953515e2bac12f081e0c67ffbe88f59a
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-18 17:29:50 +0300

    create new product functionality

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-router.get("/", (req, res) => {
-  res.send("Get all products");
-});
+// Create new product
+router.post("/", async (req, res) => {
+  try {
+
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+})
 
 module.exports = router;
