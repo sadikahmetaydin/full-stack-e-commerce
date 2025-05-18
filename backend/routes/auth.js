82308ba8a982703d4dc9c48f925598e756cd4795
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-18 16:19:31 +0300

    user login functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,4 +27,27 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// Login user
+router.post("/login", async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user) return res.status(401).json({ error: "Invalid email!" });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) return res.status(401).json({ error: "Invalid password!" });
+    res.status(200).json({
+      id: user._id,
+      email: user.email,
+      username: user.username,
+      role: user.role,
+      avatar: user.avatar
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+})
+
 module.exports = router;
