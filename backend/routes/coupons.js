64b34d74481a5cb592f2fe1f20dc2cd93f52662a
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-05-19 21:26:27 +0300

    create get all coupon, get single coupon with id and get one coupon with couponCode

--- backend/routes/coupons.js
+++ backend/routes/coupons.js
@@ -16,4 +16,48 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Get all coupons
+router.get("/", async (req, res) => {
+  try {
+
+    const coupons = await Coupon.find();
+    res.status(200).json(coupons);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Get one coupon with id
+router.get("/:couponId", async (req, res) => {
+  try {
+
+    const couponId = req.params.couponId;
+    const coupon = await Coupon.findById(couponId);
+    if(!coupon) return res.status(404).json({ error: "Coupon not found!" });
+    res.status(200).json(coupon);
+    
+  } catch (error) {
+   console.log(error);
+   res.status(500).json({ error: "Server error." }); 
+  }
+});
+
+// Get one coupon with code
+router.get("/code/:couponCode", async (req, res) => {
+  try {
+
+    const couponCode = req.params.couponCode;
+    const coupon = await Coupon.findOne({ code: couponCode });
+    if (!coupon) return res.status(404).json({ error: "Coupon not found!" });
+    const { discountPercent } = coupon;
+    res.status(200).json({ discountPercent });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+})
+
 module.exports = router;
